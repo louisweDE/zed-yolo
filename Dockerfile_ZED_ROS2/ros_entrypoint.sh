d6dd534d9eb707e40cd5dcf8f46ff42ef40e09c6
Luca <luca777744@gmail.com>
2022-01-11 10:17:11 +0100

    add ROS2 Dockerfile

--- Dockerfile_ZED_ROS2/ros_entrypoint.sh
+++ Dockerfile_ZED_ROS2/ros_entrypoint.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+#set -e
+#
+## setup ros environment
+#source "/opt/ros/$ROS_DISTRO/setup.bash"
+#exec "$@"
+#!/bin/bash
 set -e
 
-# setup ros environment
-source "/opt/ros/$ROS_DISTRO/setup.bash"
+ros_env_setup="/opt/ros/$ROS_DISTRO/setup.bash"
+echo "sourcing   $ros_env_setup"
+source "$ros_env_setup"
+
+echo "ROS_ROOT   $ROS_ROOT"
+echo "ROS_DISTRO $ROS_DISTRO"
+
 exec "$@"
